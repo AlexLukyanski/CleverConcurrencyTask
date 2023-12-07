a9a0c9601b4519c96e88202caaa8d60c45abe9c2
Aliaksei Lukyanski <alexlukyanski@gmail.com>
2023-12-08 00:01:29 +0300

    feat: add complete task solution

--- src/main/java/by/clever/concurrency/Client.java
+++ src/main/java/by/clever/concurrency/Client.java
@@ -1,4 +1,84 @@
 package by.clever.concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+
 public class Client {
+
+    private final List<Integer> clientIntegers = new ArrayList<>();
+    private int accumulator = 0;
+    private final Lock lock = new ReentrantLock();
+
+    public Client(int capacity) {
+        fillClientIntegers(capacity);
+    }
+
+    public List<Integer> getClientIntegers() {
+        try {
+            lock.lock();
+            return clientIntegers;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getClientIntegersSize() {
+        try {
+            lock.lock();
+            return clientIntegers.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getAccumulator() {
+        try {
+            lock.lock();
+            return accumulator;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Request sendRequest(int index) {
+        Random random = new Random();
+
+        try {
+            lock.lock();
+            TimeUnit.MILLISECONDS.sleep(random.ints(100, 401)
+                    .findFirst()
+                    .orElseThrow());
+            Integer value = clientIntegers.remove(index);
+            Request request = new Request(value);
+            return request;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void sumResponseData(Response response) {
+
+        int responseData = response.getData();
+
+        try {
+            lock.lock();
+            accumulator += responseData;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void fillClientIntegers(int capacity) {
+
+        IntStream.range(1, capacity + 1)
+                .forEach(clientIntegers::add);
+    }
 }
--- src/main/java/by/clever/concurrency/Request.java
+++ src/main/java/by/clever/concurrency/Request.java
@@ -1,4 +1,11 @@
 package by.clever.concurrency;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Request {
+
+    private final Integer data;
 }
